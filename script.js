2389eaee5d928fd8f827720c520ae8a9f9bf7fdc
SS Jones <SSJones.code@gmail.com>
2020-08-17 14:07:47 -0700

    This is my NEW INITIAL commit as I have concluded that I should use materialize because I have a new project that requires materialize. Also, I found a better way of formatting the row/column situation.

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1 GIVEN a weather dashboard with form inputs
+// 2 WHEN I search for a city
+// 3 THEN I am presented with current and future conditions for that city and that city is added to the search history
+// 4 WHEN I view current weather conditions for that city
+// 5 THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// 6 WHEN I view the UV index
+// 7 THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// 8 WHEN I view future weather conditions for that city
+// 9 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// 10 WHEN I click on a city in the search history
+// 11 THEN I am again presented with current and future conditions for that city
+// 12 WHEN I open the weather dashboard
+// 13 THEN I am presented with the last searched city forecast
+
+
+//==============================================================
+
+//listen for and onlclick event for the search button
+
+    //Grab the value (cityName) form the input.
+
+    //store the value in a variable using let
+
+    //query the weather API with the value from the user.(AJAX CALL).
+    
+        //.then - GRAB and store the relevent infor from the API (QUERY response) and then function { } - to make the call asyncronous
+        
+        //query the weather API for the weather index. ---- SAVE TO LAST
+
+        //display the info to the user.
